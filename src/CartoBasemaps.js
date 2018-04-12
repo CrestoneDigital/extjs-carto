e3b39cf9827ad8a9f851a8827e156633348321a7
charlierowe <charlie@silverbiology.com>
2018-04-12 15:21:02 +0530

    Example4 carto extjs link change and CartoBasemaps https change

--- src/CartoBasemaps.js
+++ src/CartoBasemaps.js
@@ -8,47 +8,47 @@ Ext.define('Carto.CartoBasemaps',{
     },{
         itemId: 'positron',
         name: 'Positron',
-        url: 'http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     },{
         itemId: 'positronLabelsBelow',
         name: 'Positron (labels below)',
-        url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'darkMatterLabelsBelow',
         name: 'Dark Matter (labels below)',
-        url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'darkMatter',
         name: 'Dark Matter',
-        url: 'http://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'darkMatterLite',
         name: 'Dark Matter (lite)',
-        url: 'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'cartoWorldEco',
         name: 'CARTO World Eco',
-        url: 'http://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png',
+        url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-eco/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'cartoWorldFlatBlue',
         name: 'CARTO World Flat Blue',
-        url: 'http://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}.png',
+        url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-flatblue/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'cartoWorldMidnightCommander',
         name: 'CARTO World Midnight Commander',
-        url: 'http://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}.png',
+        url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }, {
         itemId: 'cartoAntique',
         name: 'CARTO Antique',
-        url: 'http://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png',
+        url: 'https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     /* New basemaps */
     },{
